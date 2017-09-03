31588e79b74da5ab20e53da2b0ebff78956ec04d
XIE Yuheng <xyheme@gmail.com>
2017-09-03 18:53:11 +0800

    - use build.sh instead of makefile

--- build.sh
+++ build.sh
@@ -1,3 +1,31 @@
 #!/bin/sh
 
-clang -rdynamic -Wno-int-conversion -Wno-incompatible-pointer-types -Wno-return-type -Wunused-value jojo.c -o jojo
+#cc=gcc
+cc=clang
+w='-Wno-int-conversion -Wno-incompatible-pointer-types -Wno-return-type -Wunused-value'
+o=''
+f='-rdynamic'
+l='-ldl'
+
+tangle() {
+    echo tangle
+    ./tool/tangle.js
+    xxd -i core.jo > core.h
+}
+
+build() {
+    echo build
+    $cc $w $o $f $l jojo.c -o jojo
+}
+
+default() {
+    tangle
+    build
+}
+
+
+if [ $# -eq 0 ]; then
+    default
+else
+    $1
+fi
