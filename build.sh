74e4cb7dcbb9f29e5c3e56082ee9e50098c12124
XIE Yuheng <xyheme@gmail.com>
2017-09-07 03:18:47 +0800

    up

--- build.sh
+++ build.sh
@@ -3,14 +3,21 @@
 #cc=gcc
 cc=clang
 w='-Wno-int-conversion -Wno-incompatible-pointer-types -Wno-return-type -Wunused-value'
-o=''
+o='-O2'
 f='-rdynamic'
 l='-ldl'
 
-build() {
+tangle() {
     ./tool/tangle.js
     xxd -i core.jo > core.h
+}
+
+build() {
     $cc $w $o $f $l jojo.c -o jojo
+}
+
+fast_build() {
+    $cc $w $f $l jojo.c -o jojo
 }
 
 clean() {
@@ -20,7 +27,14 @@ clean() {
 
 default() {
     clean
+    tangle
     time build
+}
+
+test() {
+    clean
+    tangle
+    time fast_build
 }
 
 if [ $# -eq 0 ]; then
