ed8e5a6bf01e7390985e8c5e4c8835ebaa79ad4c
Xie Yuheng <xyheme@gmail.com>
2018-10-30 20:48:45 +0800

    up

--- kanren/src/lib.rs
+++ kanren/src/lib.rs
@@ -1,7 +1,18 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#![feature (uniform_paths)]
+
+#![allow (unused_parens)]
+#![allow (dead_code)]
+#![allow (unused_macros)]
+
+use std::sync::Arc;
+use error_report::{
+    Span,
+    ErrorMsg,
+    ErrorCtx,
+    ErrorInCtx,
+};
+use mexp_parser::{
+    SyntaxTable,
+    Mexp,
+    Arg,
+};
