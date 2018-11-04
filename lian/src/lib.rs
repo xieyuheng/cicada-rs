519cce110d56dd2a2c9da78a2fccf5a5a3c4251f
Xie Yuheng <xyheme@gmail.com>
2018-11-05 05:37:54 +0800

    - Term::fresh

--- lian/src/lib.rs
+++ lian/src/lib.rs
@@ -6,13 +6,16 @@
 #![allow (unused_macros)]
 #![allow (non_camel_case_types)]
 
+use std::fmt;
 use std::sync::Arc;
+use std::collections::VecDeque;
+use std::collections::HashMap;
 use uuid::Uuid;
 use dic::Dic;
 
 #[derive (Clone)]
 #[derive (Debug)]
-#[derive (PartialEq)]
+#[derive (PartialEq, Eq, Hash)]
 pub enum Term {
     Var   (VarTerm),
     Tuple (TupleTerm),
@@ -20,15 +23,31 @@ pub enum Term {
 
 #[derive (Clone)]
 #[derive (Debug)]
-#[derive (PartialEq)]
+#[derive (PartialEq, Eq, Hash)]
 pub struct VarTerm {
     name: String,
-    id: Uuid,
+    id: Id,
+}
+
+#[derive (Clone)]
+#[derive (PartialEq, Eq, Hash)]
+pub struct Id (uuid::adapter::Hyphenated);
+
+impl Id {
+    fn new () -> Self {
+        Id (Uuid::new_v4 () .to_hyphenated ())
+    }
+}
+
+impl fmt::Debug for Id {
+    fn fmt (&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write! (f, "{}", self.0)
+    }
 }
 
 #[derive (Clone)]
 #[derive (Debug)]
-#[derive (PartialEq)]
+#[derive (PartialEq, Eq, Hash)]
 pub struct TupleTerm {
     head: String,
     body: Vec <Term>,
@@ -38,7 +57,7 @@ impl Term {
     fn var (s: &str) -> Term {
         Term::Var (VarTerm {
             name: s.to_string (),
-            id: Uuid::new_v4 (),
+            id: Id::new (),
         })
     }
 }
@@ -49,6 +68,48 @@ impl Term {
             head: h.to_string (),
             body: vec,
         })
+    }
+}
+
+impl Term {
+    fn duplicate (
+        &self,
+        var_map: &mut HashMap <VarTerm, VarTerm>,
+    ) -> Term {
+        match self {
+            Term::Var (var) => {
+                if let Some (dup_var) = var_map.get (var) {
+                    Term::Var (dup_var.clone ())
+                } else {
+                    let new_var = VarTerm {
+                        name: var.name.clone (),
+                        id: Id::new (),
+                    };
+                    var_map.insert (
+                        var.clone (),
+                        new_var.clone ());
+                    Term::Var (new_var)
+                }
+            }
+            Term::Tuple (tuple) => {
+                let mut new_tuple = TupleTerm {
+                    head: tuple.head.clone (),
+                    body: Vec::new (),
+                };
+                for term in &tuple.body {
+                    new_tuple.body.push (
+                        term.duplicate (var_map));
+                }
+                Term::Tuple (new_tuple)
+            }
+        }
+    }
+}
+
+impl Term {
+    fn fresh (&self) -> Term {
+        let mut var_map = HashMap::new ();
+        self.duplicate (&mut var_map)
     }
 }
 
@@ -153,59 +214,71 @@ impl Subst {
 
 #[derive (Clone)]
 #[derive (Debug)]
-#[derive (PartialEq)]
-pub struct Disj {
-    conj_dic: Dic <Conj>,
+#[derive (PartialEq, Eq, Hash)]
+pub enum Prop {
+    Disj (Vec <Prop>),
+    Conj (Vec <Term>, Vec <Query>),
+}
+
+impl Prop {
+    fn duplicate (
+        &self,
+        var_map: &mut HashMap <VarTerm, VarTerm>,
+    ) -> Prop {
+        match self {
+            Prop::Disj (prop_vec) => {
+                let mut new_prop_vec = Vec::new ();
+                for prop in prop_vec {
+                    new_prop_vec.push (
+                        prop.duplicate (var_map));
+                }
+                Prop::Disj (new_prop_vec)
+            }
+            Prop::Conj (args, query_vec) => {
+                let mut new_args = Vec::new ();
+                for arg in args {
+                    new_args.push (
+                        arg.duplicate (var_map))
+                }
+                let mut new_query_vec = Vec::new ();
+                for query in query_vec {
+                    new_query_vec.push (
+                        query.duplicate (var_map))
+                }
+                Prop::Conj (new_args, new_query_vec)
+            }
+        }
+    }
+}
+
+impl Prop {
+    fn fresh (&self) -> Prop {
+        let mut var_map = HashMap::new ();
+        self.duplicate (&mut var_map)
+    }
 }
 
 #[derive (Clone)]
 #[derive (Debug)]
-#[derive (PartialEq)]
-pub struct Conj {
-    head: Vec <Term>,
-    body: Vec <Call>,
+#[derive (PartialEq, Eq, Hash)]
+pub struct Query {
+    name: String,
+    args: Vec <Term>,
 }
 
-#[derive (Clone)]
-#[derive (Debug)]
-#[derive (PartialEq)]
-pub enum Call {
-    Disj {
-        disj_name: String,
-        args: Vec <Term>,
-    },
-    Conj {
-        disj_name: String,
-        conj_name: String,
-        args: Vec <Term>,
-    },
-}
-
-#[derive (Clone)]
-#[derive (Debug)]
-#[derive (PartialEq)]
-pub struct Wissen {
-    disj_dic: Dic <Disj>,
-}
-
-impl Wissen {
-    fn query <'a> (
-        &'a self,
-        disj_name: &str,
-        args: Vec <Term>,
-    ) -> Solving <'a> {
-        let disj = self.disj_dic.get (disj_name) .unwrap ();
-        let frame = Frame {
-            disj_name: disj_name.to_string (),
-            disj: disj.clone (),
-            args: args,
-            index: 0,
-            backup_subst: Subst::new (),
-        };
-        Solving {
-            wissen: self,
-            trace: vec! [frame],
-            subst: Subst::new (),
+impl Query {
+    fn duplicate (
+        &self,
+        var_map: &mut HashMap <VarTerm, VarTerm>,
+    ) -> Query {
+        let mut new_args = Vec::new ();
+        for arg in &self.args {
+            new_args.push (
+                arg.duplicate (var_map));
+        }
+        Query {
+            name: self.name.clone (),
+            args: new_args,
         }
     }
 }
@@ -213,26 +286,51 @@ impl Wissen {
 #[derive (Clone)]
 #[derive (Debug)]
 #[derive (PartialEq)]
-pub struct Solving <'a> {
-    wissen: &'a Wissen,
-    trace: Vec <Frame>,
-    subst: Subst,
+pub struct Wissen {
+    prop_dic: Dic <Prop>,
+}
+
+impl Wissen {
+    fn prove <'a> (
+        &'a self,
+        query: Query,
+    ) -> Proving <'a> {
+        let trace = Trace {
+            subst: Subst::new (),
+            query_vec: vec! [Arc::new (query)],
+        };
+        let mut queue = VecDeque::new ();
+        queue.push_back (trace);
+        Proving {
+            wissen: self,
+            queue,
+        }
+    }
 }
 
 #[derive (Clone)]
 #[derive (Debug)]
 #[derive (PartialEq)]
-pub struct Frame {
-    disj_name: String,
-    disj: Disj,
-    args: Vec <Term>,
-    index: usize,
-    backup_subst: Subst,
+pub struct Proving <'a> {
+    wissen: &'a Wissen,
+    queue: VecDeque <Trace>,
 }
 
-impl <'a> Solving <'a> {
+#[derive (Clone)]
+#[derive (Debug)]
+#[derive (PartialEq)]
+pub struct Trace {
+    subst: Subst,
+    query_vec: Vec <Arc <Query>>,
+}
+
+impl <'a> Proving <'a> {
     fn next_subst (&mut self) -> Option <Subst> {
-        unimplemented! ()
+        if let Some (frame) = self.queue.pop_front () {
+            unimplemented!()
+        } else {
+            None
+        }
     }
 }
 
@@ -249,7 +347,7 @@ fn test_unify () {
             v.clone (),
             Term::tuple ("hi", vec! []),
         ]));
-    println! ("{:?}", subst.unwrap ());
+    println! ("{:#?}", subst.unwrap ());
 }
 
 #[test]
