965ad8dacdb34e70220d9bcc02cf8e3cb23dce62
Xie Yuheng <xyheme@gmail.com>
2018-11-09 07:30:16 +0800

    - Subst::unify -- for Value

--- cicada/src/lib.rs
+++ cicada/src/lib.rs
@@ -125,9 +125,9 @@ impl ToString for Id {
 #[derive (Clone)]
 #[derive (Debug)]
 #[derive (PartialEq, Eq)]
-pub enum Data {
+pub enum Value {
     Var (Var),
-    Data (String, Dic <Data>),
+    Data (String, Dic <Value>),
     TypeOfType,
 }
 
@@ -136,7 +136,151 @@ pub enum Data {
 #[derive (PartialEq, Eq)]
 pub enum Subst {
     Null,
-    Cons (Var, Data, Arc <Subst>),
+    Cons (Var, Value, Arc <Subst>),
+}
+
+impl Subst {
+    fn new () -> Self {
+        Subst::Null
+    }
+}
+
+impl Subst {
+    fn extend (&self, var: Var, value: Value) -> Self {
+        Subst::Cons (var, value, Arc::new (self.clone ()))
+    }
+}
+
+impl Subst {
+    pub fn find (&self, var: &Var) -> Option <&Value> {
+        match self {
+            Subst::Null => None,
+            Subst::Cons (
+                var1, value, next,
+            ) => {
+                if var1 == var {
+                    Some (value)
+                } else {
+                    next.find (var)
+                }
+            }
+        }
+    }
+}
+
+impl Subst {
+    pub fn walk (&self, value: &Value) -> Value {
+        match value {
+            Value::Var (var) => {
+                if let Some (new_value) = self.find (var) {
+                    self.walk (new_value)
+                } else {
+                    value.clone ()
+                }
+            }
+            _ => value.clone ()
+        }
+    }
+}
+
+impl Subst {
+    pub fn unify (
+        &self,
+        u: &Value,
+        v: &Value,
+    ) -> Option <Subst> {
+        let u = self.walk (u);
+        let v = self.walk (v);
+        match (u, v) {
+            (Value::Var (u),
+             Value::Var (v),
+            ) if u == v => {
+                Some (self.clone ())
+            }
+            (Value::Var (u), v) => {
+                if self.var_occur_p (&u, &v) {
+                    None
+                } else {
+                    Some (self.extend (u, v))
+                }
+            }
+            (u, Value::Var (v)) => {
+                if self.var_occur_p (&v, &u) {
+                    None
+                } else {
+                    Some (self.extend (v, u))
+                }
+            }
+            (Value::Data (u_name, u_dic),
+             Value::Data (v_name, v_dic),
+            ) => {
+                if u_name != v_name {
+                    return None;
+                }
+                if u_dic.len () != v_dic.len () {
+                    return None;
+                }
+                let mut subst = self.clone ();
+                let zip = u_dic.entries () .zip (v_dic.entries ());
+                for (u_entry, v_entry) in zip {
+                    if u_entry.name != v_entry.name {
+                        eprintln! ("- [warning] Subst::unify");
+                        eprintln! ("  dic mismatch");
+                        eprintln! ("  u_name = {}", u_name);
+                        eprintln! ("  v_name = {}", v_name);
+                        eprintln! ("  u_entry.name = {}", u_entry.name);
+                        eprintln! ("  v_entry.name = {}", v_entry.name);
+                        return None;
+                    } else {
+                        if let (
+                            Some (u_value),
+                            Some (v_value),
+                        ) = (&u_entry.value, &v_entry.value) {
+                            subst = subst.unify (
+                                u_value,
+                                v_value)?;
+                        } else {
+                            return None
+                        }
+                    }
+                }
+                Some (subst)
+            }
+            (u, v) => {
+                if u == v {
+                    Some (self.clone ())
+                } else {
+                    None
+                }
+            }
+        }
+    }
+}
+
+impl Subst {
+    pub fn var_occur_p (
+        &self,
+        var: &Var,
+        value: &Value,
+    ) -> bool {
+        let value = self.walk (value);
+        match value {
+            Value::Var (var1) => {
+                var == &var1
+            }
+            Value::Data (_name, dic) => {
+                for value in dic.values () {
+                    if self.var_occur_p (var, value) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            _ => {
+                false
+            }
+        }
+    }
 }
 
 #[derive (Clone)]
@@ -145,4 +289,9 @@ pub enum Subst {
 pub enum Den {
     Disj (Vec <String>, Dic <Term>),
     Conj (Dic <Term>),
+}
+
+#[test]
+fn test_unify () {
+
 }
