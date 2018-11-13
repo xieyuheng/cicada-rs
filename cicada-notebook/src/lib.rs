12305f445c7fc297e127bb03da37b80949ccdd13
Xie Yuheng <xyheme@gmail.com>
2018-11-13 12:28:06 +0800

    stab cicada-notebook

---
+++ cicada-notebook/src/lib.rs
@@ -0,0 +1,13 @@
+#![feature (uniform_paths)]
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    fn alert (s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet (name: &str) {
+    alert (&format! ("Hello, {}!", name));
+}
---
+++ cicada-notebook/web/index.js
@@ -0,0 +1,5 @@
+const js = import ("./cicada_notebook");
+
+js.then (js => {
+    js.greet ("World!");
+});
