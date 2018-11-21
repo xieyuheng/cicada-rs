3ab4d3599dc30825a2ed34e9e79cd64bf53a6a6d
Xie Yuheng <xyheme@gmail.com>
2018-11-21 18:43:08 +0800

    ready to implement time travel

--- cicada-notebook/tic-tac-toe/src/index.tsx
+++ cicada-notebook/tic-tac-toe/src/index.tsx
@@ -1,107 +1,122 @@
 import * as React from "react";
-import { useState } from 'react';
 import * as ReactDOM from "react-dom";
+import { useState } from "react";
 import "./index.css";
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button className="square">
-                {/* TODO */}
-            </button>
-        );
-    }
-}
-
-class Board extends React.Component {
-    renderSquare(i: number) {
-        return <Square />;
-    }
-
-    render() {
-        const status = 'Next player: X';
-
-        return (
-            <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
-}
-
-class Game extends React.Component {
-    render() {
-        return (
-            <div className="game">
-                <div className="game-board">
-                    <Board />
-                </div>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
-            </div>
-        );
-    }
-}
-/*
- * ReactDOM.render(
- *     <Game />,
- *     document.getElementById('root')
- * );
- *  */
-
-let Example = () => {
-    const [count, setCount] = useState(0);
-
-    return (
-        <div>
-            <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)}>
-                Click me
-            </button>
-        </div>
-    );
-};
-
-let ShoppingList = (props: {name: string}) => {
-    return (
-        <div className="shopping-list">
-            <h1>Shopping List for {props.name}</h1>
-            <ul>
-                <li>Instagram</li>
-                <li>WhatsApp</li>
-                <li>Oculus</li>
-            </ul>
-        </div>
-    );
-
-};
-
-let Root = () => {
+let Square = (props: {
+    value: string
+    onClick: () => void
+}) => {
     return <>
-        <Example />
-        <Game />
-        <ShoppingList name = "Xie Yuheng" />
+        <button className = "square" onClick = {props.onClick}>
+            {props.value}
+        </button>
     </>
 };
 
-ReactDOM.render(
-    <Root />,
-    document.getElementById('root')
-);
+let Board = () => {
+    let [squares, setSquares] = useState ([
+        "", "", "",
+        "", "", "",
+        "", "", "",
+    ]);
+
+    let [next, setNext] = useState ("X");
+
+    let handleClick = (i: number) => {
+        let s = squares.slice ();
+        if (calculateWinner (s) || squares[i]) {
+            return;
+        }
+        s [i] = next;
+        setSquares (s);
+        if (next == "X") {
+            setNext ("O");
+        } else {
+            setNext ("X");
+        }
+    };
+
+    let renderSquare = (i: number) => {
+        return (
+            <Square
+                value = {squares [i]}
+                onClick = {() => handleClick (i)}
+            />
+        );
+    };
+
+    let status;
+    let winner = calculateWinner (squares);
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Next player: " + next;
+    }
+
+    return <>
+        <div>
+            <div className="status">{status}</div>
+            <div className="board-row">
+                {renderSquare (0)}
+                {renderSquare (1)}
+                {renderSquare (2)}
+            </div>
+            <div className="board-row">
+                {renderSquare (3)}
+                {renderSquare (4)}
+                {renderSquare (5)}
+            </div>
+            <div className="board-row">
+                {renderSquare (6)}
+                {renderSquare (7)}
+                {renderSquare (8)}
+            </div>
+        </div>
+    </>
+};
+
+let Game = () => {
+    return <>
+        <div className="game">
+            <div className="game-board">
+                <Board />
+            </div>
+            <div className="game-info">
+                <div>{/* status */}</div>
+                <ol>{/* TODO */}</ol>
+            </div>
+        </div>
+    </>
+};
+
+let calculateWinner = (
+    squares: string[]
+): null | string => {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] &&
+            squares[a] === squares[b] &&
+            squares[a] === squares[c]
+        ) {
+            return squares[a];
+        }
+    }
+    return null;
+};
+
+let Root = () => <>
+    <Game />
+</>;
+
+ReactDOM.render (<Root />, document.getElementById ("root"));
