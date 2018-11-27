38438904bc168c09220fa1424d08f3458e51dbcb
Xie Yuheng <xyheme@gmail.com>
2018-11-28 03:16:56 +0800

    better notebook UI

--- cicada-notebook/web/src/index.tsx
+++ cicada-notebook/web/src/index.tsx
@@ -5,91 +5,128 @@ import { useState } from "react";
 import "./index.css";
 
 let InputBuffer = (props: {
+    focus_p: boolean
+    value: string
     onChange: (event: any) => void
     onKeyDown: (event: any) => void
 }) => {
     return <>
-        <textarea className = "Buffer"
-                  onChange = {props.onChange}
-                  onKeyDown = {props.onKeyDown}
-        >
-            {INPUT_BUFFER_MESSAGE}
-        </textarea>
+        <textarea
+            className = "InputBuffer"
+            autoFocus = {props.focus_p}
+            value = {props.value}
+            onChange = {props.onChange}
+            onKeyDown = {props.onKeyDown}
+        />
     </>
 };
 
 let OutputBuffer = (props: {
-    output: string
+    value: string
 }) => {
     return <>
-        <textarea className = "Buffer"
-                  value = {props.output}>
-        </textarea>
+        <textarea
+            className = "OutputBuffer"
+            value = {props.value}
+        />
     </>
 };
-
-let RunButton = (props: {
-    onClick: () => void
-}) => {
-    return <>
-        <button className = "RunButton"
-                onClick = {props.onClick}>
-            RUN
-        </button>
-    </>
-};
-
-const INPUT_BUFFER_MESSAGE = `\
-// - INPUT-BUFFER -
-//   - CTRL + RETURN -- run
-`;
-
-// const INPUT_BUFFER_MESSAGE = `\
-// // - INPUT-BUFFER -
-// //   - CTRL + RETURN -- run
-// //   - ALT  + RETURN -- run and new note
-// `;
-
-const OUTPUT_BUFFER_MESSAGE = `\
-// - OUTPUT-BUFFER -
-`;
 
 let Note = (props: {
+    headline: string
+    focus_p: boolean
     nb: any
+    input: string
+    output: string
+    onChange: (event: any) => void
+    onKeyDown: (event: any) => void
 }) => {
-    let [input, setInput] = useState (INPUT_BUFFER_MESSAGE);
-    let [output, setOutput] = useState (OUTPUT_BUFFER_MESSAGE);
-
-    let onKeyDown = (event: any) => {
-        if (event.ctrlKey && event.key == "Enter") {
-            setOutput (props.nb.run (input))
-        }
-        if (event.altKey && event.key == "Enter") {
-            setOutput (props.nb.run (input))
-        }
-    };
-
-    let onChange = (event: any) => {
-        setInput (event.target.value);
-    };
-
-    let onClick = () => {
-        setOutput (props.nb.run (input))
-    };
+    let out = <></>;
+    if (props.output != "") {
+        out = <OutputBuffer value = {props.output} />;
+    }
 
     return <>
         <div className = "Note">
             <hr />
-            <p><InputBuffer onChange = {onChange}
-                            onKeyDown = {onKeyDown} /></p>
-            <p><OutputBuffer output = {output} /></p>
+            <h3>{props.headline}</h3>
+            {/* - focus_p: {props.focus_p.toString ()} */}
+            <InputBuffer
+                focus_p = {props.focus_p}
+                value = {props.input}
+                onChange = {props.onChange}
+                onKeyDown = {props.onKeyDown}
+            />
+            {out}
             <hr />
         </div>
     </>
 };
 
+const WELCOME_MESSAGE = `\
+// welcome to cicada-notebook ^-^/
+// - short keys :
+//   ctrl + enter -- run
+//   alt  + enter -- run & open a note
+`;
+
+let Root = (props: {
+    nb: any
+}) => {
+    let [noteList, setNoteList] = useState ([
+        { input: WELCOME_MESSAGE, output: "" },
+    ]);
+
+    let [current, setCurrent] = useState (0);
+
+    let onKeyDown = (index: number) => (event: any) => {
+        if (event.key == "Enter" && event.ctrlKey) {
+            let length = noteList.length;
+            let list = noteList.slice (0, length);
+            let input = list [index] .input;
+            list [index] .output = props.nb.run (input);
+            setNoteList (list);
+        }
+        if (event.key == "Enter" && event.altKey) {
+            let length = noteList.length;
+            setCurrent (index + 1);
+            let list = noteList.slice (0, index + 1);
+            let succ = noteList.slice (index + 1, length);
+            let input = list [index] .input;
+            list [index] .output = props.nb.run (input);
+            list.push ({ input: "", output: "" });
+            setNoteList (list.concat (succ));
+        }
+    };
+
+    let onChange = (index: number) => (event: any) => {
+        let length = noteList.length;
+        let list = noteList.slice (0, length);
+        list [index] .input = event.target.value;
+        setNoteList (list);
+    };
+
+    let list = noteList.map ((io, index) => {
+        return <>
+            <Note
+                headline = {"#" + (index + 1) .toString ()}
+                focus_p = {index == current}
+                nb = {props.nb}
+                input = {io.input}
+                output = {io.output}
+                onChange = {onChange (index)}
+                onKeyDown = {onKeyDown (index)}
+            />
+        </>
+    });
+
+    return <>
+        {list}
+    </>
+};
+
 nb.then (nb => {
     ReactDOM.render (
-        <Note nb = {nb} />,
+        <Root nb = {nb} />,
         document.getElementById ("root"));
 });
